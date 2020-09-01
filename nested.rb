cf63974131b910e4c5a7a443b41a89591855e9a6
Evelyn Hernandez <hernandeze791@gmail.com>
2020-09-01 20:02:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,6 +107,24 @@ def changing_alan
 end
 
 def adding_to_dennis
+
+  programmer_hash =
+    {
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
 
